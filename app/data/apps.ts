b91619b9e2df5149df424b6e4b194b5f6e152e72
Warren Mason <warren@Warrens-Mac-mini.local>
2026-03-02 11:32:31 +0000

    Updated multiple files

--- app/data/apps.ts
+++ app/data/apps.ts
@@ -21,4 +21,25 @@ export interface App {
         "Multi-agent AI crypto trading system with Alpaca integration.",
       tech: ["Python", "Alpaca API", "AI Models"],
     },
+    {
+      id: "moonwalker-swap-dex",
+      title: "Moonwalker Swap Dex",
+      description:
+        "BSC/EVM-based cryptocurrecy Dex with staking, vesting and Defi",
+      tech: ["Solidity", "Next.js", "Typescript", "EVM", "GraphQl"],
+    },
+    {
+      id: "golbal-tasker",
+      title: "Global tasker an IOS/Andriod mobile App -(WIP)",
+      description:
+        "In this app it's focuse on the posters ad tasker and bridging the gap within the market at resonable rates",
+      tech: ["React-Native", "Expo", "Typescript", "Javascript", "Superbase", "PostGres DB"],
+    },
+    {
+      id: "ghost-ledger",
+      title: "Ghost Ledger an IOS/Andriod mobile App -(WIP)",
+      description:
+        "In this app it's focuse on the the individual tracking/monitoring there finances ",
+      tech: ["React-Native", "Expo", "Typescript", "Javascript", "Superbase", "PostGres DB"],
+    },
   ];
